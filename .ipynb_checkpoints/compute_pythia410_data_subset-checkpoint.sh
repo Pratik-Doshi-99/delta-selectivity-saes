88566e46df8e677d5e22b73dde076affd892bfe6
Pratik-Doshi-99 <newswanprivate@gmail.com>
2025-08-21 00:15:12 +0000

    removed cache

--- .ipynb_checkpoints/compute_pythia410_data_subset-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-cd /home/mltrain/delta-selectivity-saes
-python activations.py --model_name "EleutherAI/pythia-410m" --sae_name "EleutherAI/sae-pythia-410m-65k" --dataset_idx 1 --device cuda:0 --layers 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 --output_directory activations_pythia_410m/data_subset --model_hook_template "blocks.<layer>.hook_mlp_out" --sae_layer_template "layers.<layer>.mlp"
